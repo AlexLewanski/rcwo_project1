30fc335358ff22213d93a46679a2a451835705fc
Alex Lewanski <allewanski@gmail.com>
2024-02-29 11:22:30 -0500

    add plotting funcs and rename custom func script

--- scripts/pop_composition_results_output.R
+++ scripts/pop_composition_results_output.R
@@ -11,6 +11,7 @@ library(gridExtra)
 library(egg)
 #library(ggridges)
 library(magick)
+library(patchwork)
 
 here::here()
 
@@ -22,7 +23,7 @@ green_colfunc <- colorRampPalette(c("#add6d6", "#329999", "#287a7a", "#1e5b5b"))
 red_colfunc <- colorRampPalette(c("#ea9999", "#cc0000", "#a30000", "#7a0000"))
 
 
-pink_colfunc <- colorRampPalette(c("#fac7d3", "#f7a3b7", "#f0597d", "#bf3858"))
+pink_colfunc <- colorRampPalette(c("#f1698a", "#d85070", "#a83e57", "#90354b"))
 yellow_colfunc <- colorRampPalette(c("#ffdb19", "#e5c100"))  
 mint_colfunc <- colorRampPalette(c("#69e6c6", "#06d6a0", "#038060", "#1e5b5b"))   
 purple_colfunc <- colorRampPalette(c("#d8b2d8", "#b266b2", "#800080", "#590059"))
@@ -96,8 +97,8 @@ rcw_inbr_merge_processed <- results_list$rcw_inbr_merge %>%
             perc_90 = quantile(f_ped, prob = 0.95),
             .groups = 'drop') %>% 
   arrange(year) %>% 
-  mutate(change_color = case_when(mean > lag(mean) ~ '#329932',
-                                  mean < lag(mean) ~ '#ff4c4c',
+  mutate(change_color = case_when(mean > lag(mean) ~ '#ff4c4c',
+                                  mean < lag(mean) ~ '#329932',
                                   TRUE ~ '#4c4c4c'))
 
 fped_plot <- results_list$rcw_inbr_merge %>% 
@@ -112,7 +113,7 @@ fped_plot <- results_list$rcw_inbr_merge %>%
                colour = '#4c4c4c', linewidth = 1.15) +
   geom_point(data = rcw_inbr_merge_processed,
              aes(x = year, y = mean),
-             shape = 21, colour = rcw_inbr_merge_processed$change_color, 
+             shape = 21, color = rcw_inbr_merge_processed$change_color, 
              fill = 'white', 
              size = 2, stroke = 1.4) +
   theme_bw() +
@@ -127,7 +128,7 @@ fped_plot <- results_list$rcw_inbr_merge %>%
         axis.title.x = element_blank(),
         axis.ticks.x = element_blank(),
         plot.margin = unit(c(0.1, 1, -0.05, 1), "cm"),
-        axis.title.y = element_text(margin = margin(0, 4.75, 0, 0))
+        axis.title.y = element_text(margin = margin(0, 5.5, 0, 0))
   ) +
   scale_x_continuous(limits = c(1993.5, 2022.5), position = "top",
                      expand = c(0.01, 0)) +
@@ -149,13 +150,14 @@ top_bar <- results_list$rcw_inbr_merge %>%
              data = results_list$transloc_info_color,
              aes(x = year, y = 0, color = RCWid), size = 5.5,
              alpha = 1,
-             position = position_jitter(width = 0.37, height = 0, seed = 5345),
+             position = position_jitter(width = 0.44, height = 0, seed = 195),
              show.legend = FALSE) +
   scale_color_manual(values = setNames(results_list$transloc_info_color$source_color,# $sex_color,
                                        nm = results_list$transloc_info_color$RCWid)) +
   scale_y_continuous(expand = expansion(mult = c(0, 0), add = c(0, 1))) +
   theme_bw() +
-  theme(panel.grid.major.y = element_blank(),
+  theme(legend.title = element_text(size = 10),
+        panel.grid.major.y = element_blank(),
         panel.grid.minor = element_blank(),
         panel.grid.major.x = element_line(linetype = 'dashed', color = '#d8d8d8'),
         panel.border = element_blank(),
@@ -182,12 +184,13 @@ bottom_bar <- results_list$rcw_inbr_merge %>%
   geom_point(shape = "l", data = results_list$transloc_info_color,
              aes(x = year, y = 0, color = RCWid), size = 5.5,
              alpha = 1,
-             position = position_jitter(width = 0.37, height = 0, seed = 5345),
+             position = position_jitter(width = 0.44, height = 0, seed = 195),
              show.legend = FALSE) +
   scale_color_manual(values = setNames(results_list$transloc_info_color$source_color,# $sex_color,
                                        nm = results_list$transloc_info_color$RCWid)) +
   theme_bw() +
-  theme(panel.grid.major.y = element_blank(),
+  theme(legend.title = element_text(size = 10),
+        panel.grid.major.y = element_blank(),
         panel.grid.minor = element_blank(),
         panel.grid.major.x = element_line(linetype = 'dashed', color = '#d8d8d8'),
         panel.border = element_blank(),
@@ -210,7 +213,7 @@ transloc_ancestry <- results_list$rcws_ancestry_source_by_year %>%
                 levels = rev(c('ANF', 'CBJTC', 'FTB', 'FTS', 'ONF', 'WSF-CITRUS', 'non-transloc')))) %>% 
   ggplot() +
   geom_bar(aes(x = year, y = ancestry_prop, fill = group), 
-           color = 'white', linewidth = 0.2, width = 0.72,
+           color = 'white', linewidth = 0.15, width = 0.72,
            position = "stack", stat = "identity") + 
   #scale_fill_manual(values = c('#ededed', '#4c4c4c')) +
   scale_fill_manual(values = group_color_vec) +
@@ -223,9 +226,9 @@ transloc_ancestry <- results_list$rcws_ancestry_source_by_year %>%
         axis.line.y = element_line(color = '#4c4c4c', linewidth = 1.5),
         plot.margin = unit(c(0.25,1,0,1), "cm"),
         axis.title.x = element_text(size = 18),
-        #legend.position = 'none',
+        legend.position = 'none',
         legend.title = element_blank(),
-        axis.title.y = element_text(margin = margin(t = 0, r = 6.6, b = 0, l = 0))
+        axis.title.y = element_text(margin = margin(t = 0, r = 9, b = 0, l = 0))
   ) +
   scale_y_continuous(breaks = c(0, 0.5, 1), 
                      labels = c("0.0", "0.5", "1.0")) +
@@ -235,7 +238,64 @@ transloc_ancestry <- results_list$rcws_ancestry_source_by_year %>%
   theme(legend.key.size = unit(0.4, 'cm')) +
   coord_cartesian(clip = 'off')
 
+### custom legends ###
+fped_plot_legend <- ggdraw(cowplot::get_legend(
+  data.frame(x = 1,
+             y = 3:1,
+             label = factor(c('increase', 'no change', 'decrease'), 
+                            levels = c('increase', 'no change', 'decrease')) ) %>% 
+    ggplot() +
+    geom_point(aes(x = x,
+                   y = y,
+                   color = label),
+               shape = 21,
+               fill = 'white',
+               size = 2, stroke = 1.4) +
+    theme_bw() +
+    theme(legend.key = element_blank(),
+          legend.title = element_blank(),
+          legend.background = element_rect(fill = "transparent"),
+          panel.background = element_rect(fill = "transparent", color = "transparent")) +
+    scale_color_manual(substitute(paste('Change in mean(', italic(F)["p"], ")")),
+                       values = c('#ff4c4c', '#4c4c4c', '#329932'))
+  
+)) +
+  theme(plot.margin=unit(c(0,-5, 0, -5), "cm"),
+        panel.background = element_rect(fill = "transparent", color = "transparent"))
 
+founder_prop_legend <- results_list$rcws_founder_info %>%
+  group_by(group) %>% 
+  summarize(count = n(),
+            .groups = 'drop') %>%
+  mutate(group = factor(group, 
+                        levels = rev(c('CBJTC', 'ANF', 'FTB', 'WSF-CITRUS', 'FTS', 'ONF', 'non-transloc')))) %>% 
+  mutate(x = 0.95) %>%
+  arrange(desc(group)) %>% 
+  mutate(cumsum_count = cumsum(count) - (0.5*count)) %>% 
+  mutate(cumsum_count_adjust = cumsum_count + c(-13, -5, 0, 10, 17, 24, 0)) %>% 
+  mutate(update_label = paste0(group, ' (', count, ')')) %>% 
+  ggplot(aes(x = x, y = count, fill = group, label = count)) +
+  geom_segment(aes(x = 1.47,
+                   xend = 2,
+                   y = cumsum_count, 
+                   yend = cumsum_count_adjust,
+                   color = group),
+               linewidth = 1.1) +
+  geom_bar(position = 'stack', stat = "identity", color = 'white', linewidth = 0.15) +
+  scale_color_manual(values = group_color_vec) +
+  scale_fill_manual(values = group_color_vec) +
+  geom_text(aes(x = 2.1, y = cumsum_count_adjust, label = update_label),
+            size = 2.8,
+            hjust = 0) +
+  theme_void() +
+  theme(legend.position = 'none') +
+  coord_cartesian(clip = 'off') +
+  xlim(-0.32, 4) +
+  geom_text(aes(x = 2.76, y = 207, label = "Founder proportions"),
+            size = 3.5)
+
+
+### add in legends and RCW image ###
 rcw_pop_overview <- ggpubr::annotate_figure(egg::ggarrange(fped_plot,
                                                            top_bar,
                                                            bottom_bar,
@@ -243,19 +303,31 @@ rcw_pop_overview <- ggpubr::annotate_figure(egg::ggarrange(fped_plot,
                                                            nrow = 4,
                                                            labels = c('A', 'B', '', 'C'),
                                                            label.args = list(gp = grid::gpar(font = 2, cex = 1.2)),
-                                                           heights = c(30, 50, 50, 20)),
-                                            left = grid::textGrob("Individuals", rot = 90, hjust = 0.65, vjust = 5.2, gp = grid::gpar(cex = 1))) +
-  theme(plot.margin = unit(c(0.3, -0.8, 0, -0.45), "cm"))
+                                                           heights = c(30, 50, 50, 22)),
+                                            left = grid::textGrob("Individuals", rot = 90, hjust = 0.58, vjust = 5.2, gp = grid::gpar(cex = 1))) +
+  theme(plot.margin = unit(c(0.3, -0.8, 0, -3), "cm"))
 
 rcw_pop_overview_rcw <- rcw_pop_overview +
   cowplot::draw_image(flying_rcw,
-                      scale = 0.14,
-                      x = 0.315, y = 0.418)
+                      scale = 0.12,
+                      x = 0.325, y = 0.432) +
+  patchwork::inset_element(founder_prop_legend,
+                           left = 0.849, bottom = -0.01, right = 0.98, top = 0.20
+                           #left = 0.849, bottom = -0.01, right = 0.972, top = 0.20
+                           ) +
+  patchwork::inset_element(fped_plot_legend,
+                           #left = 0, bottom = 0.9, right = 0.38, top = 0.95,
+                           left = 0.891, bottom = 0.81, right = 0.94, top = 0.88)
+
 
 if (isTRUE(output_figs)) {
   cowplot::ggsave2(filename = here('figures', 'main_paper', 'rcw_pop_overview_fig.png'),
-                   plot = rcw_pop_overview_rcw,
+                   plot = ggdraw(rcw_pop_overview_rcw) + theme(plot.margin = unit(c(-0.3, 0.3, 0.25, -0.6), "cm")),
                    width = 10*1, height = 8.1*1, bg = 'white')
+  
+  # cowplot::ggsave2(filename = here('figures', 'main_paper', 'rcw_pop_overview_fig.png'),
+  #                  plot = rcw_pop_overview_rcw,
+  #                  width = 10*1, height = 8.1*1, bg = 'white')
 }
 
 
@@ -411,3 +483,117 @@ for (i in c('sex', 'source')) {
 
 
 
+
+
+
+
+#################################
+### CODE NOT CURRENTLY IN USE ###
+#################################
+
+# ancestry_plot_lewontin <- results_list$rcw_inbr_merge %>%
+#   mutate(count = 1) %>%
+#   group_by(year) %>%
+#   arrange(anc_prop) %>%
+#   ggplot() +
+#   geom_bar(aes(x = year, y = count, fill = anc_prop),
+#            position = "stack", stat = "identity", width = 0.98) +
+#   scale_fill_gradient(name = 'Expected\ntranslocated\nancestry', low = '#ededed', high = '#4c4c4c') +
+#   geom_point(shape = "l", data = results_list$transloc_info_color,
+#              aes(x = year, y = 0), size = 5.5, color = 'black',
+#              alpha = 1,
+#              position = position_jitter(width = 0.37, height = 0, seed = 5345),
+#              show.legend = FALSE) +
+#   scale_color_manual(values = setNames(results_list$transloc_info_color$source_color,# $sex_color,
+#                                        nm = results_list$transloc_info_color$RCWid)) +
+#   theme_bw() +
+#   theme(
+#     panel.grid.major.y = element_blank(),
+#     panel.grid.minor = element_blank(),
+#     panel.grid.major.x = element_line(linetype = 'dashed', color = '#d8d8d8'),
+#     panel.border = element_blank(),
+#     #axis.line.x = element_blank(),
+#     #axis.ticks.x = element_blank(),
+#     axis.line = element_line(color = '#4c4c4c', linewidth = 1.5)
+#   ) +
+#   #scale_y_reverse(expand = expansion(mult = c(0, 0), add = c(1, 0))) +
+#   theme(
+#     plot.margin=unit(c(-0.05,1, 0,1), "cm"),
+#     axis.title = element_text(size = 18),
+#     #axis.title.x = element_blank(),
+#     axis.text.x = element_text(size = 15)#,
+#   ) +
+#   ylab('Individuals') +
+#   xlab('Year') + 
+#   coord_cartesian(clip = 'off')
+# 
+# 
+# inbreeding_plot_lewontin <- results_list$rcw_inbr_merge %>% 
+#   ggplot() +
+#   geom_point(aes(x = year, y = f_ped),
+#              shape = 21, colour = '#b7b7b7', fill = '#e3e3e3', 
+#              size = 1, alpha = 0.8,
+#              position = position_jitter(w = 0.16, h = 0)) +
+#   geom_segment(data = rcw_inbr_merge_processed,
+#                aes(x = year, xend = year, 
+#                    y = perc_10, yend = perc_90),
+#                colour = '#4c4c4c', linewidth = 1.15) +
+#   geom_point(data = rcw_inbr_merge_processed,
+#              aes(x = year, y = mean),
+#              shape = 21, colour = rcw_inbr_merge_processed$change_color, 
+#              fill = 'white', 
+#              size = 2, stroke = 1.4) +
+#   theme_bw() +
+#   theme(axis.title.y = element_text(size = 18),
+#         axis.text.x = element_text(size = 15),
+#         panel.grid.major.y = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         panel.grid.major.x = element_line(linetype = 'dashed', 
+#                                           color = '#d8d8d8'),
+#         panel.border = element_blank(),
+#         axis.line.x = element_blank(),
+#         axis.line.y = element_line(color = '#4c4c4c', linewidth = 1.5),
+#         axis.ticks = element_line(color = '#808080', linewidth = 0.7),
+#         axis.title.x = element_blank(),
+#         axis.ticks.x = element_blank(),
+#         plot.margin = unit(c(0.1, 1, 0.2, 1), "cm"),
+#         
+#   ) +
+#   scale_x_continuous(limits = c(1993.5, 2022.5), position = "top",
+#                      expand = c(0.01, 0)) +
+#   ylab('Inbreeding') +
+#   coord_cartesian(clip = 'off')
+# 
+# 
+# lewontin_fig <- cowplot::plot_grid(inbreeding_plot_lewontin,
+#                    ancestry_plot_lewontin,
+#                    nrow = 2,
+#                    align = "v",
+#                    axis = 'tblr',
+#                    labels = c('A', 'B'),
+#                    label_x = 0.09,
+#                    label_y = c(0.89,1),
+#                    label_size = 23) +
+#   cowplot::draw_image(flying_rcw,
+#                       scale = 0.25,
+#                       x = 0.32, y = 0.35) +
+#   theme(plot.margin = unit(c(0, -1, 0, -0.7), "cm"))
+# 
+# lewontin_fig <- ggdraw(
+#   egg::ggarrange(inbreeding_plot_lewontin,
+#                  ancestry_plot_lewontin,
+#                  labels = c('A', 'B'),
+#                  label.args = list(gp = grid::gpar(font = 1, cex = 1.4)),
+#           #hjust = c(0.2, 0.9),
+#           nrow = 2,
+#           heights = c(40, 60))
+#           ) +
+#   cowplot::draw_image(flying_rcw,
+#                       scale = 0.25,
+#                       x = 0.32, y = 0.35) +
+#   theme(plot.margin = unit(c(0, -1, 0, -0.7), "cm"))
+# 
+# 
+# cowplot::ggsave2(filename = '/Users/alexlewanski/Documents/michigan_state/funding/lewontin_2024/lewontin_fig.png',
+#                  plot = lewontin_fig,
+#                  width = 9.6*1, height = 6.5*1, bg = 'white')
